4641310b8a1beb9e98ad724a5591aec268c5e3a6
kairat3 <jvckmiller@gmail.com>
2021-06-21 21:35:08 +0600

    code

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ' '
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fathomless-sea-09213.herokuapp.com', 'localhost']
 
 
 # Application definition
