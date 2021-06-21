8c4d9728db1df2308d3431c2f4b3375acda0b829
kairat3 <jvckmiller@gmail.com>
2021-06-21 21:04:37 +0600

    added static root

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -194,3 +195,5 @@ SWAGGER_SETTINGS = {
     },
     'USE_SESSION_AUTH': False
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
